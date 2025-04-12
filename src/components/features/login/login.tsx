b530d12c1a661dc806a1f3aa5c7cb6e1bc0b404c
Tojo rasii <tojorasii1619@gmail.com>
2025-04-12 13:56:32 +0300

    ajustement inscription et connexion

---
+++ src/components/features/login/login.tsx
@@ -0,0 +1,21 @@
+import SignIn from "./signin";
+import SignUp from "./signUp";
+
+
+const LoginComponent = () => {
+    return (
+        <div className="container mx-auto">
+            <div className="flex flex-col gap-5">
+                <div>
+
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 w-full">
+                    <SignIn />
+                    <SignUp />
+                </div>
+
+            </div>
+        </div>
+    );
+};
+export default LoginComponent;
