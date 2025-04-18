8729c939f53d1844e274e596746f44039ea7a698
Tojo rasii <tojorasii1619@gmail.com>
2025-04-19 00:50:28 +0300

    adjust newsletter

--- src/components/features/footer/newsletter.tsx
+++ src/components/features/footer/newsletter.tsx
@@ -3,53 +3,53 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
+
 import FooterImage from "../../../../public/images/Footer-image.png";
 
 const Newsletter = () => {
   const t = useTranslations("footer");
   const translationButton = useTranslations("button");
   return (
-    <div className="w-full h-[400px] relative overflow-hidden">
+    <div className="relative w-full h-[28em] flex items-center justify-center text-white bg-[#778B43] overflow-hidden">
+     
       <Image
         src={FooterImage}
-        className="absolute right-0"
-        alt="footer-image"
+        alt="Newsletter Background"
+        fill
+        className="object-cover object-center w-full h-full mix-blend-multiply pointer-events-none"
+        priority
       />
       <div
-        className="absolute inset-0 w-full flex flex-col justify-center align-center gap-4 px-4"
-        style={{
-          background: "linear-gradient(to right, rgba(119, 139, 67, 0.95), rgba(119, 139, 67, 0.75))",
-          backdropFilter: "brightness(1.1) saturate(1.2)",
-        }}
+        className=" w-[60%] flex flex-col justify-center align-center gap-4 max-w-full px-4"
+       
       >
         <Typography
           variant="h1"
-          className="text-center uppercase text-secondary"
+          className="text-center uppercase text-secondary font-normal"
         >
           {t("newsletter")}
         </Typography>
-        <div className="max-w-[650px] flex align-center mx-auto">
-          <Typography
-            variant="h5"
-            className="text-center text-secondary font-normal"
-          >
-            {t("newsletterMessage")}
-          </Typography>
-        </div>
-        <div className="relative flex flex-row items-center lg:w-[40%] w-[80%] mx-auto mt-3">
+        <Typography
+          variant="h5"
+          className="text-center text-secondary font-normal mb-[1em]"
+        >
+          {t("newsletterMessage")}
+        </Typography>
+     
+        <form className="max-w-full h-[4em] bg-transparent z-40 rounded-full border border-[#fff] border-solid overflow-hidden flex items-center">
           <Input
-            className="border p-5 ps-8 px-7 w-full rounded-l-full placeholder:text-white"
+            className="flex-1 text-white px-[40px] text-[1.1em] placeholder:text-[1.1em] font-semibold h-full border-none outline-none bg-transparent placeholder:text-white placeholder:opacity-70"
             placeholder={t("placeholderInput")}
           />
           <Button
             data-slot="carousel-previous"
             variant={"default"}
             size={"lg"}
-            className="bg-white cursor-pointer font-semibold rounded-r-full text-[var(--tertiary)] px-7 pe-8 py-2"
+            className="text-[#536d22] text-[1.05em] cursor-pointer rounded-none bg-[#fff] font-semibold px-[50px] h-full hover:bg-[#f5f5f5] transition-colors duration-100"
           >
             <span>{translationButton("subscribe")}</span>
           </Button>
-        </div>
+        </form>
       </div>
     </div>
   );
